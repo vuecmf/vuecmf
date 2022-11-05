2cd675ba3be5abf9c06ace77cddd797670e9b9ca
vuecmf <tulihua2004@126.com>
2022-11-05 20:41:26 +0800

    采用vite构建

--- README.md
+++ README.md
@@ -21,7 +21,7 @@ npm install -g yarn
 yarn global add @vue/cli
 ```
 
-## 使用vuecmf模板创建项目
+## 使用vuecmf-web模板创建项目
 ```
 vue create --preset vuecmf/vuecmf my-project
 ```
@@ -39,5 +39,3 @@ yarn build:test
 yarn build
 
 ```
-
-
---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build:{
+    outDir: 'admin',
+    assetsDir: 'static'
+  },
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
+  resolve:{
+    //设置路径别名
+    alias: [
+      { find: "@", replacement: resolve(__dirname, "./src") },
+    ]
+  }
+})
