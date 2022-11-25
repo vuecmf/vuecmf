d0b75026a2c30424fbd8a68ea84fd91f0c88939b
vuecmf <tulihua2004@126.com>
2022-11-25 20:48:41 +0800

    增加处理打包后vendor的js过大问题

--- generator/template/vue.config.js
+++ generator/template/vue.config.js
@@ -21,5 +21,34 @@ module.exports = {
 
         // 合并plugins
         config.plugins = [...config.plugins, ...plugins];
+    },
+    chainWebpack(config) {
+        if (process.env.NODE_ENV === 'production') {
+            config.optimization.splitChunks({
+                cacheGroups: {
+                    common: {//commons 一般是是个人定义的
+                        name: 'chunk-common', // 打包后的文件名
+                        chunks: 'initial',
+                        minChunks: 1,
+                        maxInitialRequests: 5,
+                        minSize: 0,
+                        priority: 1,
+                        reuseExistingChunk: true
+                    },
+                    vendors: {//vendor 是导入的 npm 包
+                        name: 'chunk-vendors',
+                        test: /[\\/]node_modules[\\/]/,
+                        chunks: 'initial',
+                        maxSize: 300000,
+                        maxInitialRequests: 20,
+                        priority: 2,
+                        reuseExistingChunk: true,
+                        enforce: true
+                    }
+                }
+            })
+        }
     }
+
 }
+
